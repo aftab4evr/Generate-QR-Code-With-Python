f475ddf55ed33217ce45ab423b8cc64b0d9b687e
Aftab Hussain <49341815+aftab4evr@users.noreply.github.com>
2019-07-09 20:30:41 +0530

    Create generate-qr-code.py

---
+++ generate-qr-code.py
@@ -0,0 +1,17 @@
+import qrcode
+import qrcode.image.svg
+
+method = "basic"
+
+data = "Some text that you want to store in the qrcode"
+
+if method == 'basic':
+    factory = qrcode.image.svg.SvgImage
+elif method == 'fragment':
+    factory = qrcode.image.svg.SvgFragmentImage
+elif method == 'path':
+    factory = qrcode.image.svg.SvgPathImage
+    
+img = qrcode.make(data, image_factory = factory)
+
+img.save("qrcode.svg")
